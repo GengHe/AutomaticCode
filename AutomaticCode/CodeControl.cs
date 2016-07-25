238afc1e9d4c82553963bff3ae2eb837b1300a9c
hegen <hegen@DESKTOP-QUC0N53>
2016-07-25 15:45:05 +0800

    添加项目文件。

---
+++ AutomaticCode/CodeControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticCode
+{
+    partial class DataModel
+    {
+        public List<Column> ColumnList { get; set; }
+        public string DataName { get; set; }
+        public string ClassName { get; set; }
+        //public List<string> PK_Name { get; set; }
+
+        public void IniDataBase()
+        {
+            ColumnList = new List<Column>();
+            
+           string connstr= SqlHelper.GetConnSting();
+            string sqls = $"DECLARE @P_ERR integer EXEC sp_pkeys @table_name = '{DataName}' SELECT @P_ERR as N'@P_ERR'";
+            DataTable dtt = SqlHelper.ExecuteDataset(connstr, CommandType.Text, sqls).Tables[0];
+            string sql = $"DECLARE @P_ERR integer EXEC sp_help @objname = N'{DataName}' SELECT @P_ERR as N'@P_ERR'";
+            DataTable dt = SqlHelper.ExecuteDataset(connstr, CommandType.Text, sql).Tables[1];
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Column c = new Column();
+                c.DataType = dt.Rows[i]["Type"].ToString();
+                c.Name= dt.Rows[i]["Column_name"].ToString();
+                c.Length = Convert.ToInt32(dt.Rows[i]["Length"]);
+                c.Nullable = dt.Rows[i]["Nullable"].ToString();
+                var s = dtt.Select("COLUMN_NAME='"+c.Name+"'");
+                if (s.Count() > 0)
+                {
+                    c.IsPK = true;
+                }
+                else
+                {
+                    c.IsPK = false;
+                }
+                ColumnList.Add(c);
+            }
+            
+            //PK_Name = new List<string>();
+            //for (int j = 0; j < dtt.Rows.Count; j++)
+            //{
+            //    PK_Name.Add(dtt.Rows[j]["COLUMN_NAME"].ToString());
+            //}
+        }
+    }
+    public class Column
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public int Length { get; set; }
+        public string Nullable { get; set; }
+        public bool IsPK { get; set; }
+    }
+}
---
+++ AutomaticCode/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomaticCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path3 = System.AppDomain.CurrentDomain.BaseDirectory + @"FileModel\";
+            DataModel data = new DataModel();
+            data.DataName = textBox1.Text;
+            data.ClassName = textBox2.Text;
+            data.IniDataBase();
+            
+            String pageContent = data.TransformText();
+            System.IO.File.WriteAllText(path3 + data.ClassName + ".cs", pageContent);
+            MessageBox.Show("OK");
+            
+        }
+    }
+}
